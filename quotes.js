2d6734f4a7a1896191673f302fa08c5dcd791e41
Clara Moreira <anacacamm@gmail.com>
2022-08-11 16:03:39 -0300

    initial commit

---
+++ quotes.js
@@ -0,0 +1,42 @@
+const mcflyQuotes = [
+  {
+    text: 'Not into fashion but I love the clothes she wears.',
+    song: '5 Colours in Her Hair',
+  },
+  {
+    text: 'Cause she rocked my world more than any other girl.',
+    song: 'That Girl',
+  },
+  {
+    text: 'You know that everybody likes to party on a Saturday Night.',
+    song: 'Saturday Night',
+  },
+  {
+    text: 'When she walks in the room my heart goes boom!',
+    song: 'Met This Girl',
+  },
+  {
+    text: 'Everything was going just the way I planned, the Broccoli was done.',
+    song: 'Broccoli',
+  },
+  {
+    text: 'Cause I have got you to make me feel stronger.',
+    song: 'I have Got You',
+  },
+  {
+    text: 'Yes you make my life worthwhile.',
+    song: 'All About You',
+  },
+  {
+    text: 'I guess I am stuck in this mad world.',
+    song: 'Star Girl',
+  },
+  {
+    text: 'Lets get the motion in the ocean.',
+    song: 'Please, Please',
+  },
+  {
+    text: 'Everybody is having fun to the sound of love.',
+    song: 'Transylvania',
+  },
+];
