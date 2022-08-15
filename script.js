ea5aa31005353eec4798b63b2e04e9c6107830bb
Clara Moreira <anacacamm@gmail.com>
2022-08-15 11:53:11 -0300

    create a loader

--- script.js
+++ script.js
@@ -1,21 +1,61 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
 let apiQuotes = [];
 
-function newQuote() {
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function hideLoadingSpinner() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+function showNewQuote() {
+    showLoadingSpinner();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote)
-}
 
-async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
-
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-
-        newQuote();
-    } catch (error) {
-        alert('Oops... Something went wrong!')
+    if (!quote.author) {
+      authorText.textContent = 'Unknown';
+    } else {
+      authorText.textContent = quote.author;
     }
-}
 
-getQuotes();
+    if (quote.text.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote.text;
+    hideLoadingSpinner();
+  }
+  
+  async function getQuotes() {
+    showLoadingSpinner();
+    const apiUrl = 'https://type.fit/api/quotes';
+    
+    try {
+      const response = await fetch(apiUrl);
+      apiQuotes = await response.json();
+      showNewQuote();
+    } catch (error) {
+      alert('Oops... Something went wrong!')
+    }
+  }
+  
+  function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+    window.open(twitterUrl, '_blank');
+  }
+  
+  newQuoteBtn.addEventListener('click', showNewQuote);
+  twitterBtn.addEventListener('click', tweetQuote);
+  
+  getQuotes();
